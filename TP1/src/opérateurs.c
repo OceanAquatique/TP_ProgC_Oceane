e2666dcda28c9c90a7a7c48caa3e80dae74c92a4
OceanAquatique <oceanegoncalves00@gmail.com>
2026-01-29 11:19:37 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 16;
+    int b = 3;
+
+    /* Opérateurs arithmétiques */
+    printf("Addition a + b = %d\n", a + b);
+    printf("Soustraction a - b = %d\n", a - b);
+    printf("Multiplication a * b = %d\n", a * b);
+    printf("Division a / b = %d\n", a / b);
+    printf("Modulo a %% b = %d\n", a % b);
+
+    /* Opérateurs de comparaison (booléens : 0 = faux, 1 = vrai) */
+    printf("a == b : %d\n", a == b);
+    printf("a > b : %d\n", a > b);
+
+    return 0;
+}
+
 
 
