7e897e7a8fc4925ee3eeba6c3cff05ef3245cb54
OceanAquatique <oceanegoncalves00@gmail.com>
2026-01-29 11:01:16 +0100

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
 double pi = 3.14159;
 double r = 6;
 
@@ -8,12 +8,12 @@ double aire = pi * rayon * rayon;
 double perimetre = 2 * pi * rayon;
     
 printf("L'air du cercle = %f\n", aire);
-printf("Le périmètre du cercle = %f\n", perimetre;
-}
+printf("Le périmètre du cercle = %f\n", perimetre);
 
 return 0;
+}
 
-#Explication : l'aire d'un cercle c'est aire = pi x rayon x rayon
-# Donc si le rayon = 6 on fait : 6 x 6 = 36, puis 36 x pi = 113.1
-# Donc pi * r * r
-#
+
+
+
+
