178e82f220e2263b55fd5c7610c0cc0b5ba2f50c
OceanAquatique <oceanegoncalves00@gmail.com>
2026-01-29 11:03:33 +0100

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -4,14 +4,15 @@ int main(void) {
 double pi = 3.14159;
 double r = 6;
 
-double aire = pi * rayon * rayon;
-double perimetre = 2 * pi * rayon;
+double aire = pi * r * r;
+double perimetre = 2 * pi * r;
     
-printf("L'air du cercle = %f\n", aire);
+printf("L'aire du cercle = %f\n", aire);
 printf("Le périmètre du cercle = %f\n", perimetre);
 
 return 0;
 }
+
 
 
 
